1a5f301ea83a32a3918e6868480055a166a244a8
Skylar <skylar31dhaese@gmail.com>
2024-06-13 15:50:46 -0600

    change to tomorrow

--- pages/locations_and_weather.py
+++ pages/locations_and_weather.py
@@ -55,13 +55,13 @@ for i in range(len(li_tomorrow_rain)):
         keyword = "location_name like '%Park%'"
         locations = fetch_recommended_sites(li_tomorrow_rain[i][0], keyword)
         if locations:
-            st.write('There is little to no chance of rain in ', li_tomorrow_rain[i][0], ' today. Here are some parks you can expect lots of people to be.')
+            st.write('There is little to no chance of rain in ', li_tomorrow_rain[i][0], ' tomorrow. Here are some parks you can expect lots of people to be.')
           st.dataframe(fetch_recommended_sites(li_tomorrow_rain[i][0], keyword))
     elif li_tomorrow_rain[i][4] > 25 and li_tomorrow_rain[i][4] < 75:
         keyword = "location_name like '%Museum%' or location_name like '%University%' or location_name like '%College%'"
         locations = fetch_recommended_sites(li_tomorrow_rain[i][0], keyword)
         if locations:
-          st.write('Seems like it will probably rain in ', li_tomorrow_rain[i][0], ' today. Here are some places you might still expect lots of foot traffic.' )
+          st.write('Seems like it will probably rain in ', li_tomorrow_rain[i][0], ' tomorrow. Here are some places you might still expect lots of foot traffic.' )
           st.dataframe(locations)
     elif li_tomorrow_rain[i][4] >= 75:
         st.write("It's raining everywhere. You should probably stay home.")
